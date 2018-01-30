141696508dae32a473391d37a75d84e5af0489b7
Miriam Castillo <castillo.m.mc@gmail.com>
2018-01-30 03:14:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,6 @@
-def display_rainbow(colors)
-  puts "colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']"
+def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 end
+
+#def display_rainbow(colors)
+  #puts "colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']"
+#end
